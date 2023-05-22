835bce3812e48a58e2c4a251009e4a324d5cc938
Jiyeong2 <jjyoung333@naver.com>
2023-05-22 11:37:51 +0900

    chore: best10 구현 중

--- Final_Project/src/main/java/com/book/boardmodel/BoardController.java
+++ Final_Project/src/main/java/com/book/boardmodel/BoardController.java
@@ -1,5 +1,6 @@
 package com.book.boardmodel;
 
+
 import java.util.List;
 
 import javax.inject.Inject;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.book.model.BookDTO;
 
+
 @Controller
 public class BoardController {
 	
@@ -18,14 +20,19 @@ public class BoardController {
 	
 	@RequestMapping("boardPage.go")
 	public String mainPage() {
-		
 		/*
 		 * List<BookDTO> mainList = this.dao.getBoardMainList();
 		 * 
 		 * model.addAttribute("mainList", mainList);
 		 */
 		return "board/boardPage";
-		
 	}
+	
+	@RequestMapping("boardBest.go")
+	public String mainBest() {
+		return "board/boardBest";
+	}
+	
+	
 
 }
